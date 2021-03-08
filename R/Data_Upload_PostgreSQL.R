4d0a32ccf638c13524e10019d01aa8cfe1fc3727
dsh <dsh@id.aau.dk@sds-datacrunch.aau.dk>
2021-03-08 12:49:56 +0100

    update

--- R/Data_Upload_PostgreSQL.R
+++ R/Data_Upload_PostgreSQL.R
@@ -2,67 +2,17 @@
 # Preample
 ##############################################################################
 
-### general options
-Sys.setenv(LANG = "en")
-options("scipen" = 100, "digits" = 4) # override R's tendency to use scientific notation
 
-### Clean workspace
-rm(list=ls())
-graphics.off()
-
-### setWD (maybe needs installation of rstudioapi package)
-require(rstudioapi); setwd(dirname(rstudioapi::getActiveDocumentContext()$path)); getwd()
-
-### Load packages (maybe need to be installed first)
-# Standard
-library(tidyverse) # General DS toolkit
-library(magrittr) # For advanced piping
-library(data.table) # for fast fread and other fast data transformations
-
-# Databases
-library(DBI) # GEneral R database interface
-library(RPostgreSQL) # PostgreSQL interface driver
-library(dbplyr) # for dplyr with databases
 
 ##############################################################################
 # Define functions
 ##############################################################################
 
-# Outsourced in this script
-source("000_define_functions.R")
-
-##############################################################################
-# Database connection
-##############################################################################
-
-# define driver
-drv <- dbDriver("PostgreSQL")
-
-# set up connection to existing PostgreSQL database, just plug in own details
-con <- dbConnect(drv, 
-                 dbname = "patstat2018",
-                 host = "127.0.0.1", 
-                 port = 5432,
-                 user = "YOURNAME", 
-                 password = "YOURPASSWORD")
 
 
-# # pimp up memory (to save on disk if necessary, only works on windows)
-# memory.limit(10 * 10^10)
 
-##############################################################################
-# Load adittional data: 
-##############################################################################
 
-# Dataframe with all files to import plus the corresponding database
-# NOTE: All the PATSTAT zip files need to be in the same folder, otherwise alter the path
 
-file.list <- tibble(table_id = NA,
-               data = list.files(pattern = "tls.*zip") ) %>%
-  mutate(data = gsub("\\.zip", "", .$data),
-         table_id = gsub("\\_.*", "", .$data) )
-
-table.names <- fread("PATSTAT_table_description.csv", data.table = FALSE) %>% as.tibble()
 
 ##############################################################################
 # Create table template
@@ -75,21 +25,16 @@ source("000_create_tables.R")
 # Load to PostgreSQ
 ##############################################################################
 
-# Select or disselect certain rows
-include.number <- 1:nrow(table.names) # Indicate by number the tables to include (all = 1:nrow(table.names) )
-exclude.name <- c() # Indicate by name the tables to exclude, if you want to exclude some by name
 
-# subset the table
-table.names.select <- table.names[include.number,] %>% filter(!(table_id %in% exclude.name) )
 
 # This loop imports all tables to the PostgreSQL database. You can instead also run the function sepperate for the tables
-for(i in 1:nrow(table.names.select) ) {
-  Patstat_to_PostgreSQL(files = file.list, 
+for(i in 1:nrow(table_names_select) ) {
+  Patstat_to_PostgreSQL(files = file_list, 
                         db = con,
                         append = TRUE,
                         overwrite = FALSE,
-                        tb_pat = table.names.select[i, "table_id"],
-                        tb_own = table.names.select[i, "name"]
+                        tb_pat = table_names_select[i, "table_id"],
+                        tb_own = table_names_select[i, "name"]
                         )
 }
 
